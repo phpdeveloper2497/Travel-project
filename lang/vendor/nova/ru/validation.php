f7389315cf76b57ab9c8bbb05b87ff4f2fa7f17e
uzdeveloper1 <65232715+uzdeveloper1@users.noreply.github.com>
2023-11-18 19:12:31 +0500

    language files added

---
+++ lang/vendor/nova/ru/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'attached'  => 'Значение поля :attribute уже прикреплено.',
+    'relatable' => 'Значение поля :attribute не может быть связано с этим ресурсом.',
+];
